4e9dded40e43f0d11788b966f498821592979579
Itzel-Kira <97539002+Itzel-Kira@users.noreply.github.com>
2022-02-11 13:50:54 -0600

    add end project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
+document.getElementById("botonValidar").addEventListener("click", numeroTarjeta);
 
-console.log(validator);
+function numeroTarjeta () {
+ let cardNumber = document.getElementById("tarjeta").value;
+ validator.isValid(cardNumber);
+ let isValid = validator.isValid(cardNumber);
+  respuesta (isValid)
+  validator.maskify(cardNumber);
+  let array = validator.maskify(cardNumber);
+  document.getElementById("tarjeta").value = array; 
+}
+
+function respuesta (isValid) {
+
+  if (isValid == true){
+  alert("Tú tarjeta es válida."  + " " + "\u2714");
+  }
+
+  else {
+  alert("Tú tarjeta no es válida." + " " + "\u274c");
+  }
+  
+  
+}
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,59 @@
-const validator = {
-  // ...
+const validator =  {
+    isValid (cardNumber) {
+    if (cardNumber == " " ){
+      return false;
+    }
+
+    let array = cardNumber.split(""); 
+    let arrayReversa = array.reverse();
+    for(let i=1; i < arrayReversa.length; i=i+2) {
+
+
+     let multiplicacion = arrayReversa[i];
+     let resultadoMultiplicacion = multiplicacion *2;
+     arrayReversa[i] = resultadoMultiplicacion;
+     
+
+           if (resultadoMultiplicacion > 9) {
+           let arrayNumerosPares = resultadoMultiplicacion.toString();
+           let numeros = arrayNumerosPares.split("");
+           let separarNumerosMultiplicacion1 = parseInt(numeros [0]);
+           let separarNumerosMultiplicacion2 = parseInt(numeros [1]);
+           let sumarDigitos = separarNumerosMultiplicacion1 + separarNumerosMultiplicacion2; 
+           arrayReversa[i] = sumarDigitos; 
+         
+  }
+  }
+    let numeroTotal = 0;
+    //console.log(arrayReversa);
+    for (let i=0; i <arrayReversa.length; i+=1) {
+      arrayReversa[i] = parseInt(arrayReversa[i]);
+      numeroTotal = numeroTotal + arrayReversa[i];
+      
+    }
+
+    //console.log(numeroTotal);
+    let modulo = numeroTotal%10;
+    if (modulo == 0) {
+      return true;
+    } 
+    else{
+      return false;
+    }
+ 
+},
+
+maskify(cardNumber) {
+  let array = cardNumber.split("");
+
+for (let i=0; i<array.length -4; i++){
+  array[i] = "#";
+}
+return array.join("") 
+
+}
+
 };
+
 
 export default validator;
